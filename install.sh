cc174089bdfdbd8e40c3704e32729dac0e8cd6de
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2023-09-03 22:05:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 if [[ -d fpr ]]; then
     echo frp is already downloaded
 else
-    wget -nd -O /frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz
-    tar -xf /frp/frp.tar.gz
-    rm /frp/frp.tar.gz
+    wget https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz
+    echo downloaded frp_${FRP_VERSION}_linux_amd64.tar.gz
+    tar -xf /frp/frp_${FRP_VERSION}_linux_amd64.tar.gz
+    echo extracted frp_${FRP_VERSION}_linux_amd64.tar.gz
+    rm /frp/frp_${FRP_VERSION}_linux_amd64.tar.gz
+    echo removed frp_${FRP_VERSION}_linux_amd64.tar.gz
 fi
